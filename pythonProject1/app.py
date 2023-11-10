0a084f6f1a671133ff6894f5b23522f3e016f0cd
Ivan <vannyzav907@gmail.com>
2023-11-10 19:12:27 +0300

    Add routes: "/save_book_status", "/read_books", "/book/<int:book_id>", add "is_read", "cover_url", "text_url" to Book model database.py, in Book.html make of book.name link, add checkbox, create read_books.html, book_details.html

--- pythonProject1/app.py
+++ pythonProject1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from models.database import db, Book, Genre
 
@@ -11,7 +11,10 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-    fantasy = Genre(name="фентези")
+    novel = Genre(name="Роман")
+    db.session.add(novel)
+
+    fantasy = Genre(name="Фэнтези")
     db.session.add(fantasy)
 
     Harry_potter = Book(
@@ -29,7 +32,15 @@ with app.app_context():
             genre=fantasy
         )
     )
-
+    Great_Expectations = Book(
+            id=3,
+            name="Большие Надежды",
+            author="Чарльз Диккенс",
+            genre=novel,
+            cover_url="https://img4.labirint.ru"
+                      "/rc/cf0d0bed48aa3f120ff85b793aff8a32/363x561q80/books54/530788/cover.jpg?1612697178"
+    )
+    db.session.add(Great_Expectations)
     db.session.commit()
 
 
@@ -47,6 +58,30 @@ def books_by_genre(genre_id):
         genre_name=genre.name,
         books=genre.books,
     )
+
+
+@app.route("/save_book_status", methods=["POST"])
+def save_book_status():
+    book_id = request.form.get('book_id')
+    is_read = True if request.form.get("is_read") else False
+
+    book = Book.query.get(book_id)
+    book.is_read = is_read
+    db.session.commit()
+
+    return redirect("/")
+
+
+@app.route('/read_books')
+def read_books():
+    is_read_books = Book.query.filter_by(is_read=True).all()
+    return render_template("read_books.html", books=is_read_books)
+
+
+@app.route('/book/<int:book_id>')
+def book_details(book_id):
+    book = Book.query.get(book_id)
+    return render_template('book_details.html', book=book)
 
 
 if __name__ == "__main__":
--- pythonProject1/models/database.py
+++ pythonProject1/models/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import Integer, String, ForeignKey, Boolean, Text
 from sqlalchemy.orm import Mapped, mapped_column
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, relationship
@@ -26,82 +26,14 @@ class Book(db.Model):
     author: Mapped[str] = mapped_column(String, nullable=True)
     genre_id: Mapped[int] = mapped_column(Integer, ForeignKey('genre.id', ondelete="SET NULL"))
     genre = relationship("Genre", back_populates="books")
+    is_read: Mapped[bool] = mapped_column(Boolean, default=False)
+    cover_url: Mapped[str] = mapped_column(String(200),
+                                           default="https://w7.pngwing.com"
+                                                   "/pngs/15/881/png-transparent-drawing-book"
+                                                   "-cover-shy-town-girls-book.png")
+    text_url: Mapped[str] = mapped_column(String,
+                                          default="жили были, он надеялся, она была противной, сверху упал ящер, конец"
+                                          + "мяумяу\n" * 800)
 
     def __repr__(self):
         return f'Book(name={self.name!r})'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# from typing import Type
-#
-# from sqlalchemy import Integer, String, ForeignKey
-# from sqlalchemy.orm import Mapped, mapped_column
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import DeclarativeBase, relationship
-#
-#
-# class Base(DeclarativeBase):
-#     pass
-#
-#
-# db = SQLAlchemy(model_class=Base)
-#
-#
-# class Book(db.Model):
-#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-#     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-#     author: Mapped[str] = mapped_column(String)
-#     genre_id: Mapped[int] = mapped_column(Integer, ForeignKey('genre.id', ondelete="SET NULL"))
-#     genre = relationship("Genre", back_populates="books")
-#
-#     def __repr__(self):
-#         return f'Book(name={self.name!r})'
-#
-#
-# class Genre(db.Model):
-#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-#     name: Mapped[str] = mapped_column(String)
-#     books = relationship("Book", back_populates="genre")
-#
-#     def __repr__(self):
-#         return f'Genre(name={self.name!r})'
-
-
-
-
-
